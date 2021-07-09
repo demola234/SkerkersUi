fe43ebd6e9ffb295bea3ccd797dc90dbafd2dfc4
Kolawole Ademola <64567320+demola234@users.noreply.github.com>
2021-07-09 05:53:38 +0100

    Initial Commit

---
+++ Task2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task2
+//
+//  Created by Oluwasegun Kolawole Ademola on 09/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task2/Task2App.swift
@@ -0,0 +1,17 @@
+//
+//  Task2App.swift
+//  Task2
+//
+//  Created by Oluwasegun Kolawole Ademola on 09/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Task2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
